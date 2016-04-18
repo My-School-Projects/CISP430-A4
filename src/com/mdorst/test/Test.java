539229bd0ffbcec79821120b373cae4f361ce1a3
Michael Dorst <code@mikefdorst.com>
2016-04-17 23:34:32 -0700

    Convert variables to local

--- src/com/mdorst/test/Test.java
+++ src/com/mdorst/test/Test.java
@@ -3,12 +3,10 @@ package com.mdorst.test;
 import com.mdorst.SortedTree;
 
 public class Test {
-    private static TestRunner test;
-    private static SortedTree<Integer> tree;
 
     public static void main(String[] args) {
-        test = new TestRunner();
-        tree = new SortedTree<>();
+        TestRunner test = new TestRunner();
+        SortedTree<Integer> tree = new SortedTree<>();
         test.shouldThrow(tree, "insert", null, Comparable.class, NullPointerException.class);
         test.done();
     }
