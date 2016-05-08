105bde72c11425bbcecd701690fc39bedbed3deb
Michael Dorst <code@mikefdorst.com>
2016-05-07 23:48:58 -0700

    Do file input and print ascending and descending

--- src/com/mdorst/App.java
+++ src/com/mdorst/App.java
@@ -1,7 +1,22 @@
 package com.mdorst;
 
+import com.mdorst.util.DataInput;
+
 public class App {
     public static void main(String[] args) {
-
+        SortedTree<String> dictionary = new SortedTree<>();
+        DataInput file = new DataInput("data/dictionary.txt");
+        /**
+         * Read contents of data/dictionary.txt into AVL tree
+         */
+        while (file.hasNext()) {
+            dictionary.insert(file.getLine());
+        }
+        System.out.println("Dictionary in ascending order:");
+        System.out.println("==============================");
+        dictionary.traverseAscending(System.out::println);
+        System.out.println("\nDictionary in descending order:");
+        System.out.println("===============================");
+        dictionary.traverseDescending(System.out::println);
     }
 }
