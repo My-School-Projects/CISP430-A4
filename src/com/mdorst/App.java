013050315ef53aefba8e0a4dc1ccdd291b609c7d
Michael Dorst <code@mikefdorst.com>
2016-05-11 17:15:00 -0700

    Change comment style

--- src/com/mdorst/App.java
+++ src/com/mdorst/App.java
@@ -8,9 +8,7 @@ public class App {
     public static void main(String[] args) {
         SortedTree<String> dictionary = new SortedTree<>();
         DataInput file = new DataInput("data/dictionary.txt");
-        /**
-         * Read contents of data/dictionary.txt into AVL tree
-         */
+        // Read contents of data/dictionary.txt into AVL tree
         while (file.hasNext()) {
             dictionary.insert(file.getLine());
         }
